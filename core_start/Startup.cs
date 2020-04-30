07b7c66970e7843eac2a8abc3fc75d74bae63890
少年别封 <@>
2020-04-30 13:26:05 +0800

    1

--- core_start/Startup.cs
+++ core_start/Startup.cs
@@ -73,7 +73,7 @@ namespace core_start
                 //});
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    pattern: "{controller=Department}/{action=Index}/{id?}"
                     );
             });
         }
--- core_start/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
+++ core_start/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
@@ -13,7 +13,7 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d07342dbba6d455879c6304f05121f2a26a0cfe5", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"a2bb7ed3832e4082bb5c991f31b033aeced716eb", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
